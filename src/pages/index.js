ed73476d39acc87070bdce64d72146895b648ca8
Neal Bharucha <nealbharucha@yahoo.com>
2019-10-30 15:00:52 -0500

    Clean up home page link

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello.</h1>
       <h2>My name is Neal, a full-stack developer living in Chicago</h2>
-      <p>
-        Need a developer? <Link to="/contact">Contact Me!</Link>
-      </p>
     </Layout>
   );
 };
